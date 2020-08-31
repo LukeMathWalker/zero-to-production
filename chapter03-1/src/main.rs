486ec08ffa944bdf34e9c0876a410ed23e89064b
Jes Bak Hansen <jes@copenhagensoftware.com>
2020-08-31 12:47:24 +0200

    Add comment about binding to localhost
    When running under WSL2, binding only to localhost via the 127.0.0.1 address
    only lets you access the server from within WSL2 itself. It does not allow you
    to access the server from Windows.
    
    Add a comment explaining how to work around this, by binding to all
    interfaces, instead of only to localhost.

--- chapter03-1/src/main.rs
+++ chapter03-1/src/main.rs
@@ -11,7 +11,14 @@ async fn main() -> Result<(), anyhow::Error> {
         .await
         .map_err(anyhow::Error::from)
         .with_context(|| "Failed to connect to Postgres.")?;
-    let address = format!("0.0.0.0:{}", configuration.application_port);
+
+    // Here we choose to bind explicitly to localhost, 127.0.0.1, for security
+    // reasons. This binding may cause issues in some environments. For example,
+    // it causes connectivity issues running in WSL2, where you cannot reach the
+    // server when it is bound to WSL2's localhost interface. As a workaround,
+    // you can choose to bind to all interfaces, 0.0.0.0, instead, but be aware
+    // of the security implications when you expose the server on all interfaces.
+    let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await?;
     Ok(())
