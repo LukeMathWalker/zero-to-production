e3a1996ae792c72db2c3ec3646ebcf18cffb3cbf
Luca Palmieri <lpalmieri@truelayer.com>
2020-09-21 22:24:49 +0100

    Add logger middleware.

--- chapter04/src/startup.rs
+++ chapter04/src/startup.rs
@@ -2,6 +2,7 @@ use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
+use actix_web::middleware::Logger;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
@@ -9,6 +10,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     let db_pool = Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
