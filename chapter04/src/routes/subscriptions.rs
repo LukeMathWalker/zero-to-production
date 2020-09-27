73a43bcb368c23dab98060c361a5727a5225e244
Luca Palmieri <lpalmieri@truelayer.com>
2020-09-27 13:15:09 +0100

    Introduce instrument.

--- chapter04/src/routes/subscriptions.rs
+++ chapter04/src/routes/subscriptions.rs
@@ -10,19 +10,20 @@ pub struct SubscribeRequest {
     name: String,
 }
 
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(payload, pool),
+    fields(
+        request_id=%Uuid::new_v4(),
+        email = %payload.email,
+        name = %payload.name
+    )
+)]
 pub async fn subscribe(
     payload: web::Form<SubscribeRequest>,
     pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, HttpResponse> {
-    let request_id = Uuid::new_v4();
-    let request_span = tracing::info_span!(
-        "Adding a new subscriber",
-        %request_id,
-        email = %payload.email,
-        name = %payload.name
-    );
-    let _request_span_guard = request_span.enter();
-    let query_span = tracing::info_span!("Saving new subscriber details in the database",);
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
     sqlx::query!(
         r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -37,11 +38,7 @@ pub async fn subscribe(
     .instrument(query_span)
     .await
     .map_err(|e| {
-        tracing::error!(
-            "request_id {} - Failed to execute query: {:?}",
-            request_id,
-            e
-        );
+        tracing::error!("Failed to execute query: {:?}", e);
         HttpResponse::InternalServerError().finish()
     })?;
     Ok(HttpResponse::Ok().finish())
