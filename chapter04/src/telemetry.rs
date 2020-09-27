15df7f8c3baa60200a91031632043d12ce6db411
Luca Palmieri <lpalmieri@truelayer.com>
2020-09-27 16:52:18 +0100

    Fix linter error.

--- chapter04/src/telemetry.rs
+++ chapter04/src/telemetry.rs
@@ -11,7 +11,8 @@ use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 /// We are using `impl Subscriber` as return type to avoid having to spell out the actual
 /// type of the returned subscriber, which is indeed quite complex.
 pub fn get_subscriber(name: String, env_filter: String) -> impl Subscriber + Sync + Send {
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or(EnvFilter::new(env_filter));
+    let env_filter =
+        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
     let formatting_layer = BunyanFormattingLayer::new(name, std::io::stdout);
     Registry::default()
         .with(env_filter)
