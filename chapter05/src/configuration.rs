c453631582a570e478cd06d6cc7e0b947b8a2042
Luca Palmieri <lpalmieri@truelayer.com>
2020-10-19 15:48:22 +0100

    Add configuration

--- chapter05/src/configuration.rs
+++ chapter05/src/configuration.rs
@@ -1,4 +1,4 @@
-use sqlx::postgres::PgConnectOptions;
+use sqlx::postgres::{PgConnectOptions, PgSslMode};
 
 #[derive(serde::Deserialize)]
 pub struct Settings {
@@ -19,15 +19,22 @@ pub struct DatabaseSettings {
     pub port: u16,
     pub host: String,
     pub database_name: String,
+    pub require_ssl: bool,
 }
 
 impl DatabaseSettings {
     pub fn without_db(&self) -> PgConnectOptions {
+        let ssl_mode = if self.require_ssl {
+            PgSslMode::VerifyFull
+        } else {
+            PgSslMode::Prefer
+        };
         PgConnectOptions::new()
             .host(&self.host)
             .username(&self.username)
             .password(&self.password)
             .port(self.port)
+            .ssl_mode(ssl_mode)
     }
 
     pub fn with_db(&self) -> PgConnectOptions {
@@ -38,7 +45,12 @@ impl DatabaseSettings {
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     let mut settings = config::Config::default();
 
+    // Start off by merging in the "default" configuration file
     settings.merge(config::File::with_name("configuration"))?;
+
+    // Add in settings from environment variables (with a prefix of APP and '__' as separator)
+    // E.g. `APP_APPLICATION__PORT=5001 would set `Settings.application.port`
+    s.merge(config::Environment::with_prefix("app").separator("__"))?;
 
     settings.try_into()
 }
