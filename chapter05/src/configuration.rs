06087400ee6f33ebdaa2a4d6fb5a17a977626490
Luca Palmieri <lpalmieri@truelayer.com>
2020-10-19 10:32:26 +0100

    Add address configuration.

--- chapter05/src/configuration.rs
+++ chapter05/src/configuration.rs
@@ -1,7 +1,13 @@
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16,
+    pub application: ApplicationSettings,
+}
+
+#[derive(serde::Deserialize)]
+pub struct ApplicationSettings {
+    pub port: u16,
+    pub host: String,
 }
 
 #[derive(serde::Deserialize)]
--- chapter05/src/main.rs
+++ chapter05/src/main.rs
@@ -16,7 +16,10 @@ async fn main() -> std::io::Result<()> {
         .await
         .expect("Failed to connect to Postgres.");
 
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await?;
     Ok(())
