e5a32a51a470c49b6ce7f1da01e1380ee65b8293
Luca Palmieri <lpalmieri@truelayer.com>
2020-12-08 11:42:05 +0000

    First integration test.

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -141,3 +141,34 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
         );
     }
 }
+
+#[actix_rt::test]
+async fn subscribe_returns_a_200_when_fields_are_present_but_empty() {
+    // Arrange
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
+        ("name=Ursula&email=", "empty email"),
+        ("name=Ursula&email=definitely-not-an-email", "invalid email"),
+    ];
+
+    for (body, description) in test_cases {
+        // Act
+        let response = client
+            .post(&format!("{}/subscriptions", &app.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        // Assert
+        assert_eq!(
+            200,
+            response.status().as_u16(),
+            "The API did not return a 200 OK when the payload was {}.",
+            description
+        );
+    }
+}
