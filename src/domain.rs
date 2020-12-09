2953d329ff9a914e9d7a7f3b6191a3f2d2e69f1b
Luca Palmieri <lpalmieri@truelayer.com>
2020-12-09 16:01:36 +0000

    Add SubscriberName (no methods)

---
+++ src/domain.rs
@@ -0,0 +1,1 @@
+pub struct SubscriberName(String);
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-#![allow(clippy::toplevel_ref_arg)]
 pub mod configuration;
+pub mod domain;
 pub mod routes;
 pub mod startup;
 pub mod telemetry;
