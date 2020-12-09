0eaf310f292a610c502cac92f4957af66c0b4a82
Luca Palmieri <lpalmieri@truelayer.com>
2020-12-09 17:20:15 +0000

    Add SubscriberName constructor.

--- src/domain.rs
+++ src/domain.rs
@@ -1,1 +1,39 @@
+use unicode_segmentation::UnicodeSegmentation;
+
 pub struct SubscriberName(String);
+
+impl SubscriberName {
+    /// Returns an instance of `SubscriberName` if the input satisfies all
+    /// our validation constraints on subscriber names.  
+    /// It panics otherwise.
+    pub fn parse(s: String) -> SubscriberName {
+        // `.trim()` returns a view over the input `s` without trailing
+        // whitespace-like characters.
+        // `.is_empty` checks if the view contains any character.
+        let is_empty_or_whitespace = s.trim().is_empty();
+
+        // A grapheme is defined by the Unicode standard as a "user-perceived"
+        // character: `å` is a single grapheme, but it is composed of two characters
+        // (`a` and `̊`).
+        //
+        // `graphemes` returns an iterator over the graphemes in the input `s`.
+        // `true` specifies that we want to use the extended grapheme definition set,
+        // the recommended one.
+        let is_too_long = s.graphemes(true).count() > 256;
+
+        // Iterate over all characters in the input `s` to check if any of them matches
+        // one of the characters in the forbidden array.
+        let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+        let contains_forbidden_characters = s
+            .chars()
+            .filter(|g| forbidden_characters.contains(g))
+            .count()
+            > 0;
+
+        if is_empty_or_whitespace || is_too_long || contains_forbidden_characters {
+            panic!(format!("{} is not a valid subscriber name.", s))
+        } else {
+            Self(s)
+        }
+    }
+}
