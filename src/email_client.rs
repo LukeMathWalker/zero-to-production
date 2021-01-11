b7634725db28ee0b36a1f867d078363c949de677
LukeMathWalker <contact@palmieri.com>
2021-01-11 09:37:04 +0000

    Add HttpClient.

--- src/email_client.rs
+++ src/email_client.rs
@@ -1,8 +1,17 @@
 use crate::domain::SubscriberEmail;
+use reqwest::Client;
 
-pub struct EmailClient;
+pub struct EmailClient {
+    http_client: Client,
+}
 
 impl EmailClient {
+    pub fn new() -> Self {
+        Self {
+            http_client: Client::new(),
+        }
+    }
+
     pub async fn send_email(
         &self,
         recipient: SubscriberEmail,
