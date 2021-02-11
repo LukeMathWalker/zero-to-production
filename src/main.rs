ef960053b340ef2f9bf6cee90cbd066256a88d8e
LukeMathWalker <contact@palmieri.com>
2021-02-11 08:59:55 +0000

    Update to tokio 1

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
-#[actix_rt::main]
+#[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let subscriber = get_subscriber("zero2prod".into(), "info".into());
     init_subscriber(subscriber);
