d2fd3fe1faf3cf1aee06dd7053dadb5717472815
LukeMathWalker <contact@palmieri.com>
2021-03-07 18:53:45 +0000

    Populate status column.

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -53,8 +53,8 @@ pub async fn insert_subscriber(
 ) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
-    INSERT INTO subscriptions (id, email, name, subscribed_at)
-    VALUES ($1, $2, $3, $4)
+    INSERT INTO subscriptions (id, email, name, subscribed_at, status)
+    VALUES ($1, $2, $3, $4, 'confirmed')
             "#,
         Uuid::new_v4(),
         new_subscriber.email.as_ref(),
