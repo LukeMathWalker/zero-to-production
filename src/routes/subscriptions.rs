a631932f051c6b5cbcb658fc80b86e4490bf08cc
LukeMathWalker <contact@palmieri.com>
2021-03-09 21:19:16 +0000

    Green test.

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -80,7 +80,7 @@ pub async fn insert_subscriber(
     sqlx::query!(
         r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at, status)
-    VALUES ($1, $2, $3, $4, 'confirmed')
+    VALUES ($1, $2, $3, $4, 'pending_confirmation')
             "#,
         Uuid::new_v4(),
         new_subscriber.email.as_ref(),
