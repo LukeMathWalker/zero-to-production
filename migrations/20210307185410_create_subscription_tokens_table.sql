915a227fd93e7e46a55f4b47647140479a9986a2
LukeMathWalker <contact@palmieri.com>
2021-03-11 22:09:47 +0000

    Migration.

--- migrations/20210307185410_create_subscription_tokens_table.sql
+++ migrations/20210307185410_create_subscription_tokens_table.sql
@@ -1,1 +1,7 @@
--- Add migration script here
+-- Create Subscription Tokens Table
+CREATE TABLE subscription_tokens(
+   subscription_token uuid NOT NULL,
+   subscriber_id uuid NOT NULL
+      REFERENCES subscriptions (id),
+   PRIMARY KEY (subscription_token),
+);
