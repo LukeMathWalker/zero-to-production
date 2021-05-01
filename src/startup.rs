956cbdb6f7d71832793dbd97a70c79b2041d9112
LukeMathWalker <contact@palmieri.com>
2021-05-01 16:11:16 +0100

    Update to latest actix-web.

--- src/startup.rs
+++ src/startup.rs
@@ -10,7 +10,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
     let db_pool = Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(TracingLogger)
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
