7bdc51cae90a101f0e18fe179529acda2cdd2bab
LukeMathWalker <contact@palmieri.com>
2021-05-01 16:11:16 +0100

    Update to latest actix-web.

--- src/startup.rs
+++ src/startup.rs
@@ -66,7 +66,7 @@ fn run(
     let email_client = Data::new(email_client);
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(TracingLogger)
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
