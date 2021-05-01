be141047e20db39d6214e4594d2bd49e7f6a4789
LukeMathWalker <contact@palmieri.com>
2021-05-01 15:51:06 +0100

    Use once_cell instead of lazy_static for tracing. Use custom sinks instead of filters to disable logging.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,3 +1,4 @@
+use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use uuid::Uuid;
 use wiremock::MockServer;
@@ -5,14 +6,18 @@ use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::{get_connection_pool, Application};
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
-// Ensure that the `tracing` stack is only initialised once using `lazy_static`
-lazy_static::lazy_static! {
-    static ref TRACING: () = {
-        let filter = if std::env::var("TEST_LOG").is_ok() { "debug" } else { "" };
-        let subscriber = get_subscriber("test".into(), filter.into());
+// Ensure that the `tracing` stack is only initialised once using `once_cell`
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
         init_subscriber(subscriber);
     };
-}
+});
 
 pub struct TestApp {
     pub address: String,
@@ -64,7 +69,7 @@ impl TestApp {
 }
 
 pub async fn spawn_app() -> TestApp {
-    lazy_static::initialize(&TRACING);
+    Lazy::force(&TRACING);
 
     // Launch a mock server to stand in for Postmark's API
     let email_server = MockServer::start().await;
