faaffbbd9810b80181340c56ed364d2d8c204a3e
LukeMathWalker <contact@lpalmieri.com>
2021-05-02 17:02:18 +0100

    Add error test.

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -42,6 +42,24 @@ async fn subscribe_persists_the_new_subscriber() {
 }
 
 #[actix_rt::test]
+async fn subscribe_fails_if_there_is_a_fatal_database_error() {
+    // Arrange
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    // Sabotage the database
+    sqlx::query!("ALTER TABLE subscription_tokens DROP COLUMN subscription_token;",)
+        .execute(&app.db_pool)
+        .await
+        .unwrap();
+
+    // Act
+    let response =  app.post_subscriptions(body.into()).await;
+
+    // Assert
+    assert_eq!(response.status().as_u16(), 500);
+}
+
+#[actix_rt::test]
 async fn subscribe_sends_a_confirmation_email_for_valid_data() {
     // Arrange
     let app = spawn_app().await;
