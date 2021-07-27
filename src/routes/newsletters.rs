5b87b2449d9e568ee54c8a4eec38fd4aef33d977
LukeMathWalker <contact@lpalmieri.com>
2021-07-27 09:09:20 +0100

    Add query.

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,4 +1,5 @@
 use actix_web::{web, HttpResponse};
+use sqlx::PgPool;
 
 #[derive(serde::Deserialize)]
 pub struct BodyData {
@@ -14,4 +15,26 @@ pub struct Content {
 
 pub async fn publish_newsletter(_body: web::Json<BodyData>) -> HttpResponse {
     HttpResponse::Ok().finish()
+}
+
+struct ConfirmedSubscriber {
+    name: String,
+    email: String,
+}
+
+#[tracing::instrument(name = "Adding a new subscriber", skip(pool))]
+async fn get_confirmed_subscribers(
+    pool: &PgPool,
+) -> Result<Vec<ConfirmedSubscriber>, anyhow::Error> {
+    let rows = sqlx::query_as!(
+        ConfirmedSubscriber,
+        r#"
+        SELECT email, name
+        FROM subscriptions
+        WHERE status = 'confirmed'
+        "#,
+    )
+    .fetch_all(pool)
+    .await?;
+    Ok(rows)
 }
