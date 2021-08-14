984800f1a81a563daf6ac29c004e7294a0d11c15
LukeMathWalker <contact@lpalmieri.com>
2021-08-14 23:01:18 +0100

    Random creds.

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -46,6 +46,7 @@ impl TestApp {
     pub async fn post_newsletters(&self, body: serde_json::Value) -> reqwest::Response {
         reqwest::Client::new()
             .post(&format!("{}/newsletters", &self.address))
+            .basic_auth(Uuid::new_v4().to_string(), Some(Uuid::new_v4().to_string()))
             .json(&body)
             .send()
             .await
