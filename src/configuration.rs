50a5814eb22499ccb82748aa26dd185738495157
Luca Palmieri <lpalmieri@truelayer.com>
2021-09-01 21:36:33 +0200

    Keep test suite fast by tuning timeout for tests.

--- src/configuration.rs
+++ src/configuration.rs
@@ -54,11 +54,16 @@ pub struct EmailClientSettings {
     pub base_url: String,
     pub sender_email: String,
     pub authorization_token: String,
+    pub timeout_milliseconds: u64,
 }
 
 impl EmailClientSettings {
     pub fn sender(&self) -> Result<SubscriberEmail, String> {
         SubscriberEmail::parse(self.sender_email.clone())
+    }
+
+    pub fn timeout(&self) -> std::time::Duration {
+        std::time::Duration::from_millis(self.timeout_milliseconds)
     }
 }
 
