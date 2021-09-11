f873f8a41baf3547346958ef0ac6ae9f9a842b37
Luca Palmieri <lpalmieri@truelayer.com>
2021-09-11 21:58:17 +0200

    Fix clippy lints in tests

--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -30,7 +30,7 @@ async fn the_link_returned_by_subscribe_returns_a_200_if_called() {
 
     app.post_subscriptions(body.into()).await;
     let email_request = &app.email_server.received_requests().await.unwrap()[0];
-    let confirmation_links = app.get_confirmation_links(&email_request);
+    let confirmation_links = app.get_confirmation_links(email_request);
 
     // Act
     let response = reqwest::get(confirmation_links.html).await.unwrap();
@@ -53,7 +53,7 @@ async fn clicking_on_the_confirmation_link_confirms_a_subscriber() {
 
     app.post_subscriptions(body.into()).await;
     let email_request = &app.email_server.received_requests().await.unwrap()[0];
-    let confirmation_links = app.get_confirmation_links(&email_request);
+    let confirmation_links = app.get_confirmation_links(email_request);
 
     // Act
     reqwest::get(confirmation_links.html)
