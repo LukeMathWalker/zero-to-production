e13937a89f8da75f81553b27ed43373acbe7c507
LukeMathWalker <contact@lpalmieri.com>
2021-10-26 09:53:47 +0100

    Form data processing.

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,7 +1,14 @@
 use actix_web::http::header::LOCATION;
+use actix_web::web;
 use actix_web::HttpResponse;
 
-pub async fn login() -> HttpResponse {
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    username: String,
+    password: String,
+}
+
+pub async fn login(_form: web::Form<FormData>) -> HttpResponse {
     HttpResponse::SeeOther()
         .insert_header((LOCATION, "/"))
         .finish()
