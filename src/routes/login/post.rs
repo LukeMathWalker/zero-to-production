0fc0726245299a17ced8520e994b203cc2dc6e11
LukeMathWalker <contact@lpalmieri.com>
2021-10-26 09:46:16 +0100

    Redirect to the home page.

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,5 +1,8 @@
+use actix_web::http::header::LOCATION;
 use actix_web::HttpResponse;
 
 pub async fn login() -> HttpResponse {
-    HttpResponse::Ok().finish()
+    HttpResponse::SeeOther()
+        .insert_header((LOCATION, "/"))
+        .finish()
 }
