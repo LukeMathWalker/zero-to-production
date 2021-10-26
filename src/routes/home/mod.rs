eddecc4889e6ffe470c59cffbb8b314f22d883cf
LukeMathWalker <contact@lpalmieri.com>
2021-10-26 09:06:23 +0100

    Add login form.

--- src/routes/home/mod.rs
+++ src/routes/home/mod.rs
@@ -1,9 +1,7 @@
 use actix_web::HttpResponse;
 
 pub async fn home() -> HttpResponse {
-    let body = include_str!("home.html");
-
     HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
-        .body(body)
+        .body(include_str!("home.html"))
 }
--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,5 +1,7 @@
 use actix_web::HttpResponse;
 
 pub async fn login_form() -> HttpResponse {
-    HttpResponse::Ok().finish()
+    HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(include_str!("login.html"))
 }
