c50f37c1ecf941604685186f4e45c9b0d4e6ec0a
LukeMathWalker <contact@lpalmieri.com>
2021-10-31 16:13:22 +0000

    Redirect (vanilla).

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -41,7 +41,7 @@ pub async fn login(
 pub enum LoginError {
     #[error("Authentication failed.")]
     AuthError(#[source] anyhow::Error),
-    #[error(transparent)]
+    #[error("Something went wrong.")]
     UnexpectedError(#[from] anyhow::Error),
 }
 
@@ -52,10 +52,13 @@ impl std::fmt::Debug for LoginError {
 }
 
 impl ResponseError for LoginError {
+    fn error_response(&self) -> HttpResponse {
+        HttpResponse::build(self.status_code())
+            .insert_header((LOCATION, "/login"))
+            .finish()
+    }
+
     fn status_code(&self) -> StatusCode {
-        match self {
-            LoginError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
-            LoginError::AuthError(_) => StatusCode::UNAUTHORIZED,
-        }
+        StatusCode::SEE_OTHER
     }
 }
