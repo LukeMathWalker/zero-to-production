8bf14e917e4b4b972e72cd38c33b258b835e07fe
LukeMathWalker <contact@lpalmieri.com>
2021-11-06 21:53:14 +0000

    Failures lead to a redirect.

---
+++ tests/api/login.rs
@@ -0,0 +1,17 @@
+use crate::helpers::spawn_app;
+
+#[actix_rt::test]
+async fn an_error_flash_message_is_set_on_failure() {
+    // Arrange
+    let app = spawn_app().await;
+
+    // Act
+    let login_body = serde_json::json!({
+        "username": "random-username",
+        "password": "random-password"
+    });
+    let response = app.post_login(&login_body).await;
+
+    // Assert
+    assert_eq!(response.status().as_u16(), 303);
+}
--- tests/api/main.rs
+++ tests/api/main.rs
@@ -1,5 +1,6 @@
 mod health_check;
 mod helpers;
+mod login;
 mod newsletter;
 mod subscriptions;
 mod subscriptions_confirm;
