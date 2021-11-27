1a3a8931008d11aad99d169598c134ffb608334a
LukeMathWalker <contact@lpalmieri.com>
2021-11-27 13:37:01 +0000

    Add redis configuration parameter.

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -3,14 +3,14 @@ set -x
 set -eo pipefail
 
 if ! [ -x "$(command -v psql)" ]; then
-  echo >&2 "Error: `psql` is not installed."
+  echo >&2 "Error: psql is not installed."
   exit 1
 fi
 
 if ! [ -x "$(command -v sqlx)" ]; then
-  echo >&2 "Error: `sqlx` is not installed."
+  echo >&2 "Error: sqlx is not installed."
   echo >&2 "Use:"
-  echo >&2 "    cargo install --version=0.5.5 sqlx-cli --no-default-features --features postgres"
+  echo >&2 "    cargo install --version=0.5.7 sqlx-cli --no-default-features --features postgres"
   echo >&2 "to install it."
   exit 1
 fi
--- src/configuration.rs
+++ src/configuration.rs
@@ -9,6 +9,7 @@ pub struct Settings {
     pub database: DatabaseSettings,
     pub application: ApplicationSettings,
     pub email_client: EmailClientSettings,
+    pub redis_uri: String,
 }
 
 #[derive(serde::Deserialize, Clone)]
