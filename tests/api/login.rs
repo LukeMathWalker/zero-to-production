8956ba97c651227affabdcbfb029c6e57fa5cb73
LukeMathWalker <contact@lpalmieri.com>
2021-12-16 21:49:47 +0000

    Integration test for login success.

--- tests/api/login.rs
+++ tests/api/login.rs
@@ -26,3 +26,28 @@ async fn an_error_flash_message_is_set_on_failure() {
     let html_page = response.text().await.unwrap();
     assert!(!html_page.contains(r#"<p><i>Authentication failed</i></p>"#));
 }
+
+#[actix_rt::test]
+async fn redirect_to_admin_dashboard_after_login_success() {
+    // Arrange
+    let app = spawn_app().await;
+
+    // Act - Part 1 - Login
+    let login_body = serde_json::json!({
+        "username": &app.test_user.username,
+        "password": &app.test_user.password
+    });
+    let response = app.post_login(&login_body).await;
+
+    // Assert
+    assert_eq!(response.status().as_u16(), 303);
+    assert_eq!(
+        response.headers().get("Location").unwrap(),
+        "/admin/dashboard"
+    );
+
+    // Act - Part 2 - Follow the redirect
+    let response = app.get_admin_dashboard().await;
+    let html_page = response.text().await.unwrap();
+    assert!(html_page.contains(&format!("Welcome {}", app.test_user.username)));
+}
