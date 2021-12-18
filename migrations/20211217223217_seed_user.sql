959cf2635ccc0a0d9263f2c0116d6f3a354a8b3d
LukeMathWalker <contact@lpalmieri.com>
2021-12-18 18:13:09 +0000

    Seed user.

---
+++ migrations/20211217223217_seed_user.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (user_id, username, password_hash)
+VALUES (
+    'ddf8994f-d522-4659-8d02-c1d479057be6',
+    'admin',
+    --- PHC string for `everythinghastostartsomewhere`, the seed password
+    '$argon2id$v=19$m=15000,t=2,p=1$OEx/rcq+3ts//WUDzGNl2g$Am8UFBA4w5NJEmAtquGvBmAlu92q/VQcaoL5AyJPfc8'
+);
