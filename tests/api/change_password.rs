f4f31200fe9d0fbae5706838e6c132b4344d08d3
LukeMathWalker <contact@lpalmieri.com>
2021-12-19 21:14:13 +0000

    Change password integration test.

--- tests/api/change_password.rs
+++ tests/api/change_password.rs
@@ -148,11 +148,11 @@ async fn changing_password_works() {
     // Act - Part 3 - Follow the redirect
     let response = app.get_change_password().await;
     let html_page = response.text().await.unwrap();
-    assert!(!html_page.contains(r#"<p><i>Your password has been changed successfully!</i></p>"#));
+    assert!(html_page.contains(r#"<p><i>Your password has been changed successfully!</i></p>"#));
 
     // Act - Part 4 - Logout
     let response = app.post_logout().await;
-    assert!(!html_page.contains(r#"<p><i>You have successfully logged out.</i></p>"#));
+    assert!(html_page.contains(r#"<p><i>You have successfully logged out.</i></p>"#));
 
     // Act - Part 5 - Login using the new password
     let login_body = serde_json::json!({
