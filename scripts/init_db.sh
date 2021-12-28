0c2e299eb06b8e8d8fc71bc0fd940b98ed529c66
Luca Palmieri <lpalmieri@truelayer.com>
2021-12-28 12:18:38 +0100

    Do not use backticks - they lead to the script trying to execute the quoted command.

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -3,12 +3,12 @@ set -x
 set -eo pipefail
 
 if ! [ -x "$(command -v psql)" ]; then
-  echo >&2 "Error: `psql` is not installed."
+  echo >&2 "Error: psql is not installed."
   exit 1
 fi
 
 if ! [ -x "$(command -v sqlx)" ]; then
-  echo >&2 "Error: `sqlx` is not installed."
+  echo >&2 "Error: sqlx is not installed."
   echo >&2 "Use:"
   echo >&2 "    cargo install --version=0.5.5 sqlx-cli --no-default-features --features postgres"
   echo >&2 "to install it."
