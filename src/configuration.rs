974e855b4db6fb32d497ab2282091d4c6f7f00a7
Luca Palmieri <lpalmieri@truelayer.com>
2022-01-02 16:50:14 +0100

    Remove direct 'log' dependency

--- src/configuration.rs
+++ src/configuration.rs
@@ -45,7 +45,7 @@ impl DatabaseSettings {
 
     pub fn with_db(&self) -> PgConnectOptions {
         let mut options = self.without_db().database(&self.database_name);
-        options.log_statements(log::LevelFilter::Trace);
+        options.log_statements(tracing::log::LevelFilter::Trace);
         options
     }
 }
