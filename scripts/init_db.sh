71a6c5f74cc8c0cc5fb51297d0d4200a983b5d49
Joseph Livesey <52332499+suchapalaver@users.noreply.github.com>
2022-04-15 08:22:55 -0400

    Correct comment on DB custom and default settings

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -19,7 +19,7 @@ fi
 DB_USER="${POSTGRES_USER:=postgres}"
 # Check if a custom password has been set, otherwise default to 'password'
 DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
-# Check if a custom password has been set, otherwise default to 'newsletter'
+# Check if a custom database name has been set, otherwise default to 'newsletter'
 DB_NAME="${POSTGRES_DB:=newsletter}"
 # Check if a custom port has been set, otherwise default to '5432'
 DB_PORT="${POSTGRES_PORT:=5432}"
