291b65493ec5957f4bb3b7907e9c0436c842923f
Luca P <contact@lpalmieri.com>
2022-07-05 17:47:21 +0100

    Update all dependencies

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ async fn main() -> std::io::Result<()> {
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPoolOptions::new()
-        .connect_timeout(std::time::Duration::from_secs(2))
+        .acquire_timeout(std::time::Duration::from_secs(2))
         .connect_lazy_with(configuration.database.with_db());
 
     let address = format!(
