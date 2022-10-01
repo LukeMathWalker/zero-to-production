7e3ee6cfc3435ee09ac8fd465866c7d2f678e678
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2022-10-01 18:07:52 +0100

    Fix CI

--- src/configuration.rs
+++ src/configuration.rs
@@ -31,7 +31,10 @@ impl DatabaseSettings {
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     let settings = config::Config::builder()
-        .add_source(config::File::new("configuration.yaml", config::FileFormat::Yaml))
+        .add_source(config::File::new(
+            "configuration.yaml",
+            config::FileFormat::Yaml,
+        ))
         .build()?;
     settings.try_deserialize::<Settings>()
 }
