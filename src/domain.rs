5690fa3b1ab6b74ef3f45609f0904fa6a2d79191
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2023-02-18 18:20:05 +0000

    Update Cargo.lock

--- src/domain.rs
+++ src/domain.rs
@@ -49,7 +49,7 @@ impl AsRef<str> for SubscriberName {
 #[cfg(test)]
 mod tests {
     use crate::domain::SubscriberName;
-    use claim::{assert_err, assert_ok};
+    use claims::{assert_err, assert_ok};
 
     #[test]
     fn a_256_grapheme_long_name_is_valid() {
