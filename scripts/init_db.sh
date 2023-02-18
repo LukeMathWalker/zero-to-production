0924a7069ce103da46908514f54d05cdc0f7e980
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2023-02-18 16:24:47 +0000

    Update CI.

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -56,7 +56,7 @@ done
 
 >&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations now!"
 
-export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
+export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
 sqlx database create
 sqlx migrate run
 
