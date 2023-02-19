af9ea818a4d2fbffa0ea4dc9f3d6e018b1e8cdd4
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2023-02-19 18:38:12 +0000

    Simplify assertion

--- tests/api/login.rs
+++ tests/api/login.rs
@@ -21,5 +21,5 @@ async fn an_error_flash_message_is_set_on_failure() {
 
     // Act - Part 3 - Reload the login page
     let html_page = app.get_login_html().await;
-    assert!(!html_page.contains("<p><i>Authentication failed</i></p>"));
+    assert!(!html_page.contains("Authentication failed"));
 }
