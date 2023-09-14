d585096633be3fb1e457c55da248ad73ea80e506
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2023-09-14 09:43:32 +0200

    Update sqlx to 0.7

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,7 +1,6 @@
 use secrecy::{ExposeSecret, Secret};
 use serde_aux::field_attributes::deserialize_number_from_string;
 use sqlx::postgres::{PgConnectOptions, PgSslMode};
-use sqlx::ConnectOptions;
 use std::convert::{TryFrom, TryInto};
 
 #[derive(serde::Deserialize)]
@@ -44,9 +43,7 @@ impl DatabaseSettings {
     }
 
     pub fn with_db(&self) -> PgConnectOptions {
-        let mut options = self.without_db().database(&self.database_name);
-        options.log_statements(tracing::log::LevelFilter::Trace);
-        options
+        self.without_db().database(&self.database_name)
     }
 }
 
