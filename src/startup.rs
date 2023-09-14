baf60d4796a613c39f8f16e8a7a43c243eb24320
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2023-09-14 09:43:32 +0200

    Update sqlx to 0.7

--- src/startup.rs
+++ src/startup.rs
@@ -55,9 +55,7 @@ impl Application {
 }
 
 pub fn get_connection_pool(configuration: &DatabaseSettings) -> PgPool {
-    PgPoolOptions::new()
-        .acquire_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy_with(configuration.with_db())
+    PgPoolOptions::new().connect_lazy_with(configuration.with_db())
 }
 
 pub struct ApplicationBaseUrl(pub String);
