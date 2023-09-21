2ac5ce66b31eabec999d798fbe7c6b2bc00bbad5
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2023-09-21 08:06:30 +0200

    Formatting

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,7 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPoolOptions::new()
-        .connect_lazy_with(configuration.database.with_db());
+    let connection_pool = PgPoolOptions::new().connect_lazy_with(configuration.database.with_db());
 
     let address = format!(
         "{}:{}",
