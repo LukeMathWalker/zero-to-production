b2de856b3daecb5442cb045942fadd7533117518
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2023-09-21 08:24:47 +0200

    Formatting

--- src/startup.rs
+++ src/startup.rs
@@ -55,8 +55,7 @@ impl Application {
 }
 
 pub fn get_connection_pool(configuration: &DatabaseSettings) -> PgPool {
-    PgPoolOptions::new()
-        .connect_lazy_with(configuration.with_db())
+    PgPoolOptions::new().connect_lazy_with(configuration.with_db())
 }
 
 pub struct ApplicationBaseUrl(pub String);
