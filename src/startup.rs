8aab2126e5646d2c5466833d76e5f799ad884572
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2023-09-21 08:09:23 +0200

    Formatting

--- src/startup.rs
+++ src/startup.rs
@@ -14,7 +14,7 @@ use actix_web::{web, App, HttpServer};
 use actix_web_flash_messages::storage::CookieMessageStore;
 use actix_web_flash_messages::FlashMessagesFramework;
 use actix_web_lab::middleware::from_fn;
-use secrecy::{Secret, ExposeSecret};
+use secrecy::{ExposeSecret, Secret};
 use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::net::TcpListener;
