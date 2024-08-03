470205e5243415c4b03ef0ad1062f7b064a057b4
Luis Jesús Díaz Manzo <luis@drconopoima.com>
2024-08-03 17:41:55 +0200

    quickcheck fix error rust fake the trait bound  is not satisfied

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -22,9 +22,12 @@ impl AsRef<str> for SubscriberEmail {
 #[cfg(test)]
 mod tests {
     use super::SubscriberEmail;
-    use claim::assert_err;
+    use claims::assert_err;
     use fake::faker::internet::en::SafeEmail;
     use fake::Fake;
+    use rand::{rngs::StdRng, SeedableRng};
+    extern crate quickcheck;
+    extern crate quickcheck_macros;
 
     #[test]
     fn empty_string_is_rejected() {
@@ -49,7 +52,8 @@ mod tests {
 
     impl quickcheck::Arbitrary for ValidEmailFixture {
         fn arbitrary<G: quickcheck::Gen>(g: &mut G) -> Self {
-            let email = SafeEmail().fake_with_rng(g);
+            let mut rng = StdRng::seed_from_u64(u64::arbitrary(g));
+            let email = SafeEmail().fake_with_rng(&mut rng);
             Self(email)
         }
     }
