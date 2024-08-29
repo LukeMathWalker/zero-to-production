82cb8ed097a269d7ccfb76314d77d02d46250176
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2024-08-29 17:42:37 +0200

    Don't pin fake and quickcheck to older versions

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -25,6 +25,8 @@ mod tests {
     use claims::assert_err;
     use fake::faker::internet::en::SafeEmail;
     use fake::Fake;
+    use rand::rngs::StdRng;
+    use rand::SeedableRng;
 
     #[test]
     fn empty_string_is_rejected() {
@@ -48,8 +50,10 @@ mod tests {
     struct ValidEmailFixture(pub String);
 
     impl quickcheck::Arbitrary for ValidEmailFixture {
-        fn arbitrary<G: quickcheck::Gen>(g: &mut G) -> Self {
-            let email = SafeEmail().fake_with_rng(g);
+        fn arbitrary(g: &mut quickcheck::Gen) -> Self {
+            let mut rng = StdRng::seed_from_u64(u64::arbitrary(g));
+            let email = SafeEmail().fake_with_rng(&mut rng);
+
             Self(email)
         }
     }
