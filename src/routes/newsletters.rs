131526e6fda7600f26c50541047500b11d20e19f
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2024-08-29 14:54:52 +0200

    Update dependencies

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -100,7 +100,7 @@ pub async fn publish_newsletter(
     request: HttpRequest,
 ) -> Result<HttpResponse, PublishError> {
     let credentials = basic_authentication(request.headers()).map_err(PublishError::AuthError)?;
-    tracing::Span::current().record("username", &tracing::field::display(&credentials.username));
+    tracing::Span::current().record("username", tracing::field::display(&credentials.username));
     let user_id = validate_credentials(credentials, &pool)
         .await
         .map_err(|e| match e {
@@ -108,7 +108,7 @@ pub async fn publish_newsletter(
             AuthError::UnexpectedError(_) => PublishError::UnexpectedError(e.into()),
         })?;
 
-    tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
+    tracing::Span::current().record("user_id", tracing::field::display(&user_id));
 
     let subscribers = get_confirmed_subscribers(&pool).await?;
     for subscriber in subscribers {
