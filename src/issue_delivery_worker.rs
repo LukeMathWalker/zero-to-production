f70b2b9d00f2896d37488c1bb839fed545054f33
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2024-08-29 14:54:52 +0200

    Update dependencies

--- src/issue_delivery_worker.rs
+++ src/issue_delivery_worker.rs
@@ -48,8 +48,8 @@ pub async fn try_execute_task(
     }
     let (transaction, issue_id, email) = task.unwrap();
     Span::current()
-        .record("newsletter_issue_id", &display(issue_id))
-        .record("subscriber_email", &display(&email));
+        .record("newsletter_issue_id", display(issue_id))
+        .record("subscriber_email", display(&email));
     match SubscriberEmail::parse(email.clone()) {
         Ok(email) => {
             let issue = get_issue(pool, issue_id).await?;
--- src/utils.rs
+++ src/utils.rs
@@ -11,7 +11,7 @@ where
 
 // Return a 400 with the user-representation of the validation error as body.
 // The error root cause is preserved for logging purposes.
-pub fn e400<T: std::fmt::Debug + std::fmt::Display>(e: T) -> actix_web::Error
+pub fn e400<T>(e: T) -> actix_web::Error
 where
     T: std::fmt::Debug + std::fmt::Display + 'static,
 {
