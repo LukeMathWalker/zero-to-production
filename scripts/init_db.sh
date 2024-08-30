aa9c00de51360b9174031fca9b54748b67ed5e48
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2024-08-30 11:28:12 +0200

    Don't require the user to have psql installed

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,11 +2,6 @@
 set -x
 set -eo pipefail
 
-if ! [ -x "$(command -v psql)" ]; then
-  echo >&2 "Error: psql is not installed."
-  exit 1
-fi
-
 if ! [ -x "$(command -v sqlx)" ]; then
   echo >&2 "Error: sqlx is not installed."
   echo >&2 "Use:"
@@ -36,23 +31,30 @@ then
     echo >&2 "    docker kill ${RUNNING_POSTGRES_CONTAINER}"
     exit 1
   fi
+  CONTAINER_NAME="postgres_$(date '+%s')"
   # Launch postgres using Docker
   docker run \
       -e POSTGRES_USER=${DB_USER} \
       -e POSTGRES_PASSWORD=${DB_PASSWORD} \
       -e POSTGRES_DB=${DB_NAME} \
+      --health-cmd="pg_isready -U ${DB_USER} || exit 1" \
+      --health-interval=1s \
+      --health-timeout=5s \
+      --health-retries=5 \
       -p "${DB_PORT}":5432 \
       -d \
-      --name "postgres_$(date '+%s')" \
+      --name "${CONTAINER_NAME}" \
       postgres -N 1000
       # ^ Increased maximum number of connections for testing purposes
+      
+  until [ \
+    "$(docker inspect -f "{{.State.Health.Status}}" ${CONTAINER_NAME})" == \
+    "healthy" \
+  ]; do     
+    >&2 echo "Postgres is still unavailable - sleeping"
+    sleep 1 
+  done
 fi
-
-# Keep pinging Postgres until it's ready to accept commands
-until PGPASSWORD="${DB_PASSWORD}" psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
-  >&2 echo "Postgres is still unavailable - sleeping"
-  sleep 1
-done
 
 >&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations now!"
 
