7fed08d396a556696f25b597961b9bcc8b2fba0a
Luca Palmieri <20745048+LukeMathWalker@users.noreply.github.com>
2024-08-31 17:09:08 +0200

    Fix 'without db' confusion

--- src/configuration.rs
+++ src/configuration.rs
@@ -32,7 +32,7 @@ pub struct DatabaseSettings {
 }
 
 impl DatabaseSettings {
-    pub fn without_db(&self) -> PgConnectOptions {
+    pub fn connect_options(&self) -> PgConnectOptions {
         let ssl_mode = if self.require_ssl {
             PgSslMode::Require
         } else {
@@ -44,10 +44,7 @@ impl DatabaseSettings {
             .password(self.password.expose_secret())
             .port(self.port)
             .ssl_mode(ssl_mode)
-    }
-
-    pub fn with_db(&self) -> PgConnectOptions {
-        self.without_db().database(&self.database_name)
+            .database(&self.database_name)
     }
 }
 
